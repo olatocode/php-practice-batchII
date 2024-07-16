1e0bf9e3cc320027eea08b7647e1fd9effa188b2
olatocodes <tobi@kenrow.online>
2024-07-16 10:49:19 +0100

    functions updated

--- Learning/functions.php
+++ Learning/functions.php
@@ -11,42 +11,66 @@ array_push($course, "python", "Javascript");
 
 // user define functions
 
-function greet($name)
-{
-echo "Good morning," . $name . "<br>";
-}
+// function greet($name)
+// {
+// echo "Good morning," . $name . "<br>";
+// }
 // greet("Tobi");
 
-function addNum($num1, $num2){
-    echo $num1 + $num2;
-}
+// function addNum($num1, $num2){
+//     echo $num1 + $num2;
+// }
 
 // addNum(5, 6);
 
-function skills(){
- $course = ["php", "laravel", "react", "vue"];
- echo print_r($course);
-}
+// function skills(){
+//  $course = ["php", "laravel", "react", "vue"];
+//  echo print_r($course);
+// }
 // skills();
 
 
 
-function calSales($price, $qty){
-    $bread = 400;
-    $tea = 200;
-    if ($bread > 350) {
-        $breadPrice = $price * $qty;
+// function calSales($price, $qty){
+//     $bread = 400;
+//     $tea = 200;
+//     if ($bread > 350) {
+//         $breadPrice = $price * $qty;
     
-      if ($tea >= 200) {
-            $teaPrice = $price * $qty;
+//       if ($tea >= 200) {
+//             $teaPrice = $price * $qty;
             
-        }
-        $totalSales = $breadPrice + $teaPrice;
-        echo $totalSales;
-    }
-    }
+//         }
+//         $totalSales = $breadPrice + $teaPrice;
+//         echo $totalSales;
+//     }
+//     }
 
-calSales(400, 2)
+// calSales(400, 2);
+
+// $products = "tea";
+// $price = 200;
+// $qty = 1;
+
+
+function calSales ($products, $price, $qty){
+    $products =  $price * $qty;
+    $totalSales = $products;
+    echo $totalSales;
+}
+
+$bread = calSales("bread", 400, 2);
+$tea = calSales("Tea", 200, 2);
+echo "Bread: $bread";
+echo "tea: $tea";
+
+
+function Sales($bread, $tea){
+  $total = $bread + $tea;
+ echo $total;
+}
+
+Sales($bread, $tea);
 
 
 
